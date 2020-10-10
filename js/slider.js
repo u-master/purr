9dfd3abd6e7d683741801f41bf915759a4fa6fce
u-master <to-umaster@mail.ru>
2020-10-10 12:32:24 +0600

    add dots switching, fix slides shifting

--- js/slider.js
+++ js/slider.js
@@ -1,37 +1,66 @@
-function sliderRun({ sliderId, viewNum }) {
+function sliderRun({
+  sliderId,
+  viewNum = 1,
+  animationDelay = 20,
+  buttonStyles = {},
+  dotStyles = {},
+  activeDotStyles = {},
+}) {
+  // States
   let currentIndex = 0;
   let animationProcess = 'idle';
-  const sizeControl = 15;
-  const sizeDot = 10;
-  const animationDelay = 20;
-  const animationMultiplyer = 5;
 
+  // Default styles
+  const unitedButtonStyles = {
+    border: '1px solid black',
+    borderRadius: '50%',
+    color: 'black',
+    fontSize: '12px',
+    width: '15px',
+    height: '15px',
+    textDecoration: 'none',
+    lineHeight: '15px',
+    textAlign: 'center',
+    ...buttonStyles,
+  };
+
+  const unitedDotStyles = {
+    display: 'block',
+    borderRadius: '50%',
+    width: '10px',
+    height: '10px',
+    backgroundColor: 'gray',
+    margin: '0 5px',
+    ...dotStyles,
+  };
+
+  const unitedActiveDotStyles = {
+    ...unitedDotStyles,
+    backgroundColor: 'silver',
+    ...dotStyles,
+  };
+
+  // View
   const slider = document.getElementById(sliderId);
   if (slider === null) return;
   const slides = [...slider.children];
   const slidesLen = slides.length;
 
   const wrapper = createTopWrapper();
-  const controls = createControls(sizeControl);
+  const controls = createControls(unitedButtonStyles);
   const viewport = createViewport();
-  const dots = createDots(sizeDot, slider.children.length);
-  const slidesContainer = createSlidesContainer();
+  const dots = createDots(slider.children.length, unitedDotStyles);
+  const slidesContainer = createSlidesContainer(viewNum);
 
   setSliderStyles(slider);
   rebuildSlider(slider, wrapper, dots, controls, viewport, slidesContainer);
   renderSlides(slidesContainer, slides, currentIndex, viewNum);
+  setDotStyles(dots, currentIndex, unitedActiveDotStyles);
 
-  function shiftSlidesContainer(currentMargin, direction) {
-    const marginLeft = currentMargin - direction * animationMultiplyer;
-    slidesContainer.style.marginLeft = `${marginLeft}%`;
-    if (marginLeft >= 0 || marginLeft <= -100) {
-      currentIndex = (currentIndex + direction + slidesLen) % slidesLen;
-      renderSlides(slidesContainer, slides, currentIndex, viewNum);
-      animationProcess = 'ended';
-      return;
-    }
-    setTimeout(shiftSlidesContainer, animationDelay, marginLeft, direction);
-  }
+  // Controller
+  const animationMultiplyer = 10 / viewNum;
+  const startMarginLeft = -100 / viewNum;
+  const stopMarginLeft = 2 * startMarginLeft;
 
   controls.prevButton.addEventListener('click', function (elem) {
     elem.preventDefault();
@@ -40,7 +69,7 @@ function sliderRun({ sliderId, viewNum }) {
       return;
     }
     animationProcess = 'running';
-    setTimeout(shiftSlidesContainer, animationDelay, -50, -1);
+    setTimeout(shiftSlidesContainer, animationDelay, startMarginLeft, -1);
   });
 
   controls.nextButton.addEventListener('click', function (elem) {
@@ -50,115 +79,121 @@ function sliderRun({ sliderId, viewNum }) {
       return;
     }
     animationProcess = 'running';
-    setTimeout(shiftSlidesContainer, animationDelay, -50, 1);
+    setTimeout(shiftSlidesContainer, animationDelay, startMarginLeft, 1);
   });
-}
 
-/*----------------------*/
+  /** Shift effect animation */
+  function shiftSlidesContainer(currentMargin, direction) {
+    const marginLeft = currentMargin - direction * animationMultiplyer;
+    if (marginLeft >= 0 || marginLeft <= stopMarginLeft) {
+      setDotStyles(dots, currentIndex, unitedDotStyles);
+      currentIndex = (currentIndex + direction + slidesLen) % slidesLen;
+      renderSlides(slidesContainer, slides, currentIndex, viewNum);
+      slidesContainer.style.marginLeft = `${startMarginLeft}%`;
+      setDotStyles(dots, currentIndex, unitedActiveDotStyles);
+      animationProcess = 'ended';
+      return;
+    }
+    slidesContainer.style.marginLeft = `${marginLeft}%`;
+    setTimeout(shiftSlidesContainer, animationDelay, marginLeft, direction);
+  }
 
-/** Tune styles for slider */
-function setSliderStyles(slider) {
-  slider.style.display = 'flex';
-  slider.style.flexDirection = 'column';
-  slider.style.justifyContent = 'flex-start';
-  slider.style.alignItems = 'stretch';
-}
+  /** Tune styles for slider */
+  function setSliderStyles(slider) {
+    slider.style.display = 'flex';
+    slider.style.flexDirection = 'column';
+    slider.style.justifyContent = 'flex-start';
+    slider.style.alignItems = 'stretch';
+  }
 
-/** Create top elements wrapper (contains controls and slides) */
-function createTopWrapper() {
-  const wrapper = document.createElement('div');
-  wrapper.style.display = 'flex';
-  wrapper.style.justifyContent = 'space-between';
-  wrapper.style.alignItems = 'center';
-  return wrapper;
-}
+  /** Create top elements wrapper (contains controls and slides) */
+  function createTopWrapper() {
+    const wrapper = document.createElement('div');
+    wrapper.style.display = 'flex';
+    wrapper.style.justifyContent = 'space-between';
+    wrapper.style.alignItems = 'center';
+    return wrapper;
+  }
 
-/** Create slides container with buffer */
-function createSlidesContainer() {
-  const slidesContainer = document.createElement('div');
-  slidesContainer.style.display = 'flex';
-  slidesContainer.style.justifyContent = 'space-around';
-  slidesContainer.style.alignItems = 'center';
-  slidesContainer.style.width = '200%';
-  slidesContainer.style.marginLeft = '-50%';
-  return slidesContainer;
-}
+  /** Create slides container with buffer */
+  function createSlidesContainer(viewNum) {
+    const slidesContainer = document.createElement('div');
+    slidesContainer.style.display = 'flex';
+    slidesContainer.style.justifyContent = 'space-around';
+    slidesContainer.style.alignItems = 'center';
+    slidesContainer.style.width = `${100 + 200 / viewNum}%`;
+    slidesContainer.style.marginLeft = `${-100 / viewNum}%`;
+    return slidesContainer;
+  }
 
-/** Create controls */
-function createControls(sizeControl) {
-  const prevButton = document.createElement('a');
-  prevButton.href = '#';
-  prevButton.style.border = '1px solid black';
-  prevButton.style.borderRadius = '50%';
-  prevButton.style.color = 'black';
-  prevButton.style.fontSize = `${Math.floor(sizeControl * 0.75)}px`;
-  prevButton.style.width = `${sizeControl}px`;
-  prevButton.style.height = `${sizeControl}px`;
-  prevButton.style.textDecoration = 'none';
-  prevButton.style.lineHeight = `${sizeControl}px`;
-  prevButton.style.textAlign = 'center';
-  prevButton.style.flexShrink = '0';
-  const nextButton = prevButton.cloneNode(false);
-  prevButton.innerText = '‹';
-  prevButton.style.marginRight = '10px';
-  nextButton.innerText = '›';
-  nextButton.style.marginLeft = '10px';
-  return { prevButton, nextButton };
-}
+  /** Create controls */
+  function createControls(buttonStyles) {
+    const prevButton = document.createElement('a');
+    Object.assign(prevButton.style, buttonStyles);
+    prevButton.style.flexShrink = '0';
+    prevButton.href = '#';
+    const nextButton = prevButton.cloneNode(false);
+    prevButton.innerText = '‹';
+    prevButton.style.marginRight = '10px';
+    nextButton.innerText = '›';
+    nextButton.style.marginLeft = '10px';
+    return { prevButton, nextButton };
+  }
 
-/** Create viewport for slides. It hides unnecessary slides. */
-function createViewport() {
-  const viewport = document.createElement('div');
-  viewport.style.width = '100%';
-  viewport.style.overflow = 'hidden';
-  return viewport;
-}
+  /** Create viewport for slides. It hides unnecessary slides. */
+  function createViewport() {
+    const viewport = document.createElement('div');
+    viewport.style.width = '100%';
+    viewport.style.overflow = 'hidden';
+    return viewport;
+  }
 
-/** Create dots */
-function createDots(sizeDot, numDots) {
-  const dotsContainer = document.createElement('div');
-  const dots = [...Array(numDots)].map(() => {
-    const dot = document.createElement('a');
-    dot.href = '#';
-    dot.style.display = 'block';
-    dot.style.borderRadius = '50%';
-    dot.style.width = `${sizeDot}px`;
-    dot.style.height = `${sizeDot}px`;
-    dot.style.backgroundColor = 'silver';
-    dot.style.margin = `0 ${Math.floor(sizeDot / 2)}px`;
-    return dot;
-  });
-  dotsContainer.append(...dots);
-  dotsContainer.style.display = 'flex';
-  dotsContainer.style.justifyContent = 'center';
-  dotsContainer.style.width = '100%';
-  dotsContainer.style.paddingTop = '10px';
-  dotsContainer.style.flexShrink = '0';
-  return dotsContainer;
-}
+  /** Create dots */
+  function createDots(numDots, dotStyles) {
+    const dotsContainer = document.createElement('div');
+    const dots = [...Array(numDots)].map(() => {
+      const dot = document.createElement('a');
+      Object.assign(dot.style, dotStyles);
+      dot.href = '#';
+      return dot;
+    });
+    dotsContainer.append(...dots);
+    dotsContainer.style.display = 'flex';
+    dotsContainer.style.justifyContent = 'center';
+    dotsContainer.style.width = '100%';
+    dotsContainer.style.paddingTop = '10px';
+    dotsContainer.style.flexShrink = '0';
+    return dotsContainer;
+  }
 
-/** Make clean slider, then add slides viewport, control buttons and dots */
-function rebuildSlider(
-  slider,
-  wrapper,
-  dots,
-  { prevButton, nextButton },
-  viewport,
-  slidesContainer,
-) {
-  viewport.append(slidesContainer);
-  wrapper.append(prevButton, viewport, nextButton);
-  slider.innerHTML = '';
-  slider.append(wrapper, dots);
-}
+  /** Set dot styles (switch between active and simple dot styles) */
+  function setDotStyles(dotsContainer, currentIndex, styles) {
+    Object.assign(dotsContainer.children[currentIndex].style, styles);
+  }
 
-/** Render slides to slidesContainer */
-function renderSlides(slidesContainer, slides, currentIndex, viewNum) {
-  const slidesNum = viewNum + 2;
-  const slidesToContainer = Array(slidesNum);
-  const startIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
-  for (let i = 0; i < slidesNum; i += 1)
-    slidesToContainer[i] = slides[(startIndex + i) % slides.length].cloneNode(true);
-  slidesContainer.innerHTML = '';
-  slidesContainer.append(...slidesToContainer);
+  /** Make clean slider, then add slides viewport, control buttons and dots */
+  function rebuildSlider(
+    slider,
+    wrapper,
+    dots,
+    { prevButton, nextButton },
+    viewport,
+    slidesContainer,
+  ) {
+    viewport.append(slidesContainer);
+    wrapper.append(prevButton, viewport, nextButton);
+    slider.innerHTML = '';
+    slider.append(wrapper, dots);
+  }
+
+  /** Render slides to slidesContainer */
+  function renderSlides(slidesContainer, slides, currentIndex, viewNum) {
+    const slidesNum = viewNum + 2;
+    const slidesToContainer = Array(slidesNum);
+    const startIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
+    for (let i = 0; i < slidesNum; i += 1)
+      slidesToContainer[i] = slides[(startIndex + i) % slides.length].cloneNode(true);
+    slidesContainer.innerHTML = '';
+    slidesContainer.append(...slidesToContainer);
+  }
 }
